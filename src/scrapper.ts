3a1f6116c91f491219493178bad211d6234c2ca4
rohan <rohan.shuvam@gmail.com>
2022-04-17 12:37:15 +0530

    Changed search html scrapping to using internal api

--- src/scrapper.ts
+++ src/scrapper.ts
@@ -1,28 +1,27 @@
 import axios from "axios";
+import qs from "qs";
 
 // @ts-ignore
 export async function search(query:string):Promise<Array<AnimeSearch>>{
-    const res = await axios.get(`https://www2.kickassanime.ro/search?q=${query}`);
-    const searchHTML = res.data;
-    const DividedHTML = searchHTML.split('\n');
-    let pageData:string;
-    DividedHTML.forEach((line:string)=>{
-        // @ts-ignore
-        if(line.includes('appUrl')){
-            pageData = line;
-        }
+    const data = qs.stringify({
+        "keyword": query
     });
 
-    const segregateData = pageData!.slice(156,-16);
-    const Data = JSON.parse(segregateData);
-    return Data.animes;
+    const config = {
+        headers: {
+            'Host': 'www2.kickassanime.ro',
+            'Origin': 'https://www2.kickassanime.ro',
+            'Referer': 'https://www2.kickassanime.ro',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        }
+    };
+    const animeData = await axios.post("https://www2.kickassanime.ro/api/anime_search", data, config);
+    return animeData.data;
 }
 
 
 export interface AnimeSearch {
     name: string,
     slug: string,
-    slug_id: string,
-    poster: string,
     image: string
 }
